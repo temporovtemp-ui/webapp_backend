1aed3f601e80813eb160058828083d4165efdb26
temporovtemp <temporovtemp@gmail.com>
2025-11-08 16:44:04 +0300

    A complete simple web server.

--- src/main/java/com/webserver/Main.java
+++ src/main/java/com/webserver/Main.java
@@ -1,13 +1,17 @@
 package com.webserver;
 
+import com.webserver.endpoints.HelloWorldEndpoint;
+import com.webserver.handlers.RoutingHandler;
+
 public class Main {
     public static void main(String[] args) {
         int port = 8080;
 
         Router router = new Router();
-        router.putEntry();
+        router.putEntry("/hello", HttpMethod.GET, new HelloWorldEndpoint());
+        RoutingHandler routingHandler = new RoutingHandler(router);
 
-        HttpServer server = new HttpServer(port);
+        HttpServer server = new HttpServer(port, routingHandler);
         server.run();
     }
 }
